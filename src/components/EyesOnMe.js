c45dffe4919d39543663515633baadce772c17d5
Christopher Houghton <ChrisHoughton@ChristopherMBP.fios-router.home>
2021-11-19 10:14:14 -0500

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,26 @@
-// Code EyesOnMe Component Here
+import React from "react";
+
+export default class EyesOnMe extends React.Component {
+    
+    state = {
+        text: "Next"
+      }
+
+    changeText = (newText) => {
+      this.setState({text: newText }); 
+    }
+    
+    render() {
+    let { text } = this.state;
+      return <button value={'this'}
+      onFocus={() => {
+        console.log('Good!');
+        this.changeText('Good!') 
+      }}
+      onBlur={() => {
+        console.log('Hey! Eyes on me!');
+        this.changeText('Hey! Eyes on me!')
+      }}
+      >{text}</button>;
+    }
+  }
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,9 @@
-// Code Keypad Component Here
+import React from "react";
+
+export default class Keypad extends React.Component {
+    render() {
+      return <input type="password" onKeyUp={function () {
+          return console.log('Entering password...');
+      }}></input> ;
+    }
+  }
